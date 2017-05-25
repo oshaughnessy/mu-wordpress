07e51ec8c9ed2fd5b46e0770dbedf8f4352fd0dd
O'Shaughnessy Evans <oshaughnessy.evans@stelligent.com>
2017-05-25 11:02:55 -0700

    update html content -- still not used

--- html/wp-config-local.php
+++ html/wp-config-local.php
@@ -1,3 +1,13 @@
 <?php
 
+// Turn debugging on
 define('WP_DEBUG', true);
+
+// Tell WordPress to log everything to /wp-content/debug.log
+define('WP_DEBUG_LOG', true);
+
+// Turn off the display of error messages on your site
+define('WP_DEBUG_DISPLAY', false);
+
+// For good measure, you can also add the follow code, which will hide errors from being displayed on-screen
+@ini_set('display_errors', 0);
