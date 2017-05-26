f150cb017663f428d29720424785ae1acdc5658f
O'Shaughnessy Evans <oshaughnessy.evans@stelligent.com>
2017-05-26 12:10:13 -0700

    remove WP_DEBUG from wp-config-local.php

--- html/wp-config-local.php
+++ html/wp-config-local.php
@@ -1,7 +1,9 @@
 <?php
 
-// Turn debugging on
-define('WP_DEBUG', true);
+// See "WORDPRESS_DEBUG" in mu.yml to configure WP_DEBUG,
+// so that it can be set per environment.
+// -- DO NOT SET WP_DEBUG HERE --
+//define('WP_DEBUG', true);
 
 // Tell WordPress to log everything to /wp-content/debug.log
 define('WP_DEBUG_LOG', true);
