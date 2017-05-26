61e1134bc0df29a7c6607a72789edd6ae2ac9f20
O'Shaughnessy Evans <oshaughnessy.evans@stelligent.com>
2017-05-26 14:38:19 -0700

    remove WP_DEBUG settings from local config.php -- can't set them after wp-config.php already has

--- html/wp-config-local.php
+++ html/wp-config-local.php
@@ -1,14 +1,5 @@
 <?php
 
-// See "WORDPRESS_DEBUG" in mu.yml to configure WP_DEBUG,
-// so that it can be set per environment.
-// -- DO NOT SET WP_DEBUG HERE --
-//define('WP_DEBUG', true);
-
-// Tell WordPress to log everything to /wp-content/debug.log
-define('WP_DEBUG_LOG', true);
-
-// Turn off the display of error messages on your site
-define('WP_DEBUG_DISPLAY', true);
-
-@ini_set('display_errors', 1);
+/*
+ * Add your own WordPress config settings to this file
+ */
